f8f3a802104fa4599db85da1647c64dd82bf6153
YoranT <108663797+YoranT@users.noreply.github.com>
2025-07-02 19:33:27 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -13,6 +13,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     for device in ajax.devices:
         sensors.append(AjaxBatterySensor(device))
 
+        if device.get("type") == "doorProtect":
+            sensors.append(AjaxOpenCloseSensor(device))
+
+    for hub in ajax.hubs:
+        sensors.append(AjaxHubStatusSensor(hub))
+
     async_add_entities(sensors)
 
 class AjaxBatterySensor(Entity):
@@ -32,3 +38,38 @@ class AjaxBatterySensor(Entity):
     @property
     def unit_of_measurement(self):
         return "%"
+
+class AjaxOpenCloseSensor(Entity):
+    def __init__(self, device):
+        self._device = device
+        self._attr_name = f"{device['name']} Deurstatus"
+        self._attr_unique_id = f"{device['id']}_open_close"
+
+    @property
+    def state(self):
+        return "open" if self._device.get("isOpened") else "closed"
+
+    @property
+    def device_class(self):
+        return "opening"
+
+class AjaxHubStatusSensor(Entity):
+    def __init__(self, hub):
+        self._hub = hub
+        self._attr_name = f"{hub['name']} Alarmstatus"
+        self._attr_unique_id = f"{hub['id']}_alarm_status"
+
+    @property
+    def state(self):
+        return self._hub.get("securityStatus")
+
+    @property
+    def icon(self):
+        status = self.state
+        if status == "armed":
+            return "mdi:shield-lock"
+        elif status == "disarmed":
+            return "mdi:shield-off"
+        elif status == "partial":
+            return "mdi:shield-half-full"
+        return "mdi:shield-alert"
